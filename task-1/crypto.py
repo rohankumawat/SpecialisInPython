75fce784a18a872d6ff8c3752fde85f458599ae8
Rohan Kumawat <kumawatrohan@gmail.com>
2020-08-26 23:41:22 +0530

    Update crypto.py

--- task-1/crypto.py
+++ task-1/crypto.py
@@ -6,7 +6,7 @@ print()
 print("\t\t\t\t\tHi! I'm your personal voice assistant Crypto...")
 print("\t\t\t\t\t```````````````````````````````````````````````")
 
-pyttsx3.speak("Hi I'm crypto, your personal voice assistant! What can I for you?")
+pyttsx3.speak("Hi I'm crypto, your personal voice assistant! What can I do for you?")
 pyttsx3.speak("Choose a method which you want to prefer to do these partiular tasks!")
 
 print("""
@@ -22,13 +22,16 @@ if int(method) == 1:
 	while True:
 		print("""
 			Press 1: To show date.
-			Press 2: To show calendar.
-			Press 3: To change the console of the color.
-			Press 4: To open Google Chrome.
-			Press 5: To go to Youtube.
-			Press 6: To open Gmail.
-			Press 7: Make Chrome your default browser
-			Press 8: Make Opera your default browser
+			Press 2: To show time.
+			Press 3: To open calculator.
+			Press 4: To open WordPad.
+			Press 5: To open Character Map.
+			Press 6: To change the console of the color.
+			Press 7: To open Google Chrome.
+			Press 8: To go to Youtube.
+			Press 9: To open Gmail.
+			Press 10: Make Chrome your default browser
+			Press 11: Make Opera your default browser
 		""")
 
 		print("Enter your choice:")
@@ -37,8 +40,14 @@ if int(method) == 1:
 		if int(ch) == 1:
 			os.system("date")	#date
 		elif int(ch) == 2:
-			os.system("cal")	#calendar
+			os.system("time")	#time
 		elif int(ch) == 3:
+			os.system("calc")	#calculator
+		elif int(ch) == 4:
+			os.system("write")	#wordpad
+		elif int(ch) == 5:
+			os.system("charmap")	#character-map
+		elif int(ch) == 6:
 			print("""
 			What color do you want your console to look like?
 			0: Black 						8: Gray
@@ -51,16 +60,16 @@ if int(method) == 1:
 			8: White						F: Bright White
 			""")
 			color = input()	#color_choice
-			os.system("color " + color)
-		elif int(ch) == 4:
-			os.system("chrome")	#chrome
-		elif int(ch) == 5:
-			os.system("start https://www.youtube.com")	#Youtube
-		elif int(ch) == 6:
-			os.system("start https://www.gmail.com")	#Gmail
+			os.system("color {}".format(color))
 		elif int(ch) == 7:
-			os.system("chrome.exe --make-default-browser")	#ChromeDefaultBrowser
+			os.system("chrome")	#chrome
 		elif int(ch) == 8:
+			os.system("start https://www.youtube.com")	#Youtube
+		elif int(ch) == 9:
+			os.system("start https://www.gmail.com")	#Gmail
+		elif int(ch) == 10:
+			os.system("chrome.exe --make-default-browser")	#ChromeDefaultBrowser
+		elif int(ch) == 11:
 			os.system("launcher.exe --make-default-browser")	#OperaDefaultBrowser	
 
 elif int(method) == 2:
